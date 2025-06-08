61584961c83639e85fba054e0a4d3d80db176ca4
Payal Kumari <151648321+payalrawal123@users.noreply.github.com>
2025-06-08 16:58:54 +0530

    Update Search.jsx

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
 const Search = () => {
+    const [searchValue, setSearchValue] = useState('');
+   const getMovieRequest = async () => {
+        const url = `http://www.omdbapi.com/?s=star wars&apikey=263d22d8`;
+
+        const response = await fetch(url);
+        const responseJson = await response.json();
+
+        if (responseJson.Search) {
+            setMovies(responseJson.Search);
+        }
+    };
   return (
     <div>
-      
+       <div className='col col-sm-4'>
+            <input
+                className='form-control'
+                value={value}
+                onChange={getMovieRequest}
+                placeholder='Type to search...'
+            ></input>
+        </div>
     </div>
   )
 }
